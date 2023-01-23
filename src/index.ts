58c231ed79f9e5ec264db6dba918e60e88ed340e
Chris Wu <chris@faros.ai>
2023-01-23 14:49:21 -0800

    Add source code and tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-// Code
+export class Utils {
+  static urlWithoutTrailingSlashes(url: string): string {
+    return new URL(url).toString().replace(/\/{1,10}$/, "");
+  }
+
+  static toStringList(value?: string | string[]): string[] {
+    if (!value) {
+      return [];
+    }
+    if (Array.isArray(value)) {
+      return value;
+    }
+    return value
+      .split(",")
+      .map((x) => x.trim())
+      .filter((p) => p);
+  }
+
+  static toDate(val: Date | string | number | undefined): Date | undefined {
+    if (typeof val === "number") {
+      return new Date(val);
+    }
+    if (!val) {
+      return undefined;
+    }
+    return new Date(val);
+  }
+}
