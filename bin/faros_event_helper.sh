222a50c68176848fd587fb56539fd6494f14a066
Matthew Tovbin <tovbinm@gmail.com>
2023-01-23 13:08:44 -0800

    wire commit info

--- bin/faros_event_helper.sh
+++ bin/faros_event_helper.sh
@@ -11,11 +11,18 @@ export FAROS_RUN_PIPELINE="$(hostname)"
 export FAROS_RUN_ORG='faros-ai'
 export FAROS_RUN_SOURCE='Mock'
 export FAROS_RUN_ID="$(git branch --show-current)"
+export FAROS_VCS_ORG='faros-ai'
+export FAROS_VCS_REPO='lighthouse'
+export FAROS_VCS_SOURCE='Mock'
+
+NOW="$(jq -n 'now * 1000 | floor')"
 
 function parseFlags() {
     while (($#)); do
         case "$1" in
             --step) run_step="$2" && shift 2 ;;
+            --commit-sha) commit_sha="$2" && shift 2 ;;
+            --commit-message) commit_message="$2" && shift 2 ;;
             *)
                 POSITIONAL+=("$1") # save it in an array for later
                 shift ;;
@@ -43,6 +50,9 @@ function processType() {
                 shift ;;
             RUN_STEP_FAILED)
                 run_step_failed
+                shift ;;
+            SEND_COMMIT)
+                send_commit
                 shift ;;
         esac
     done
@@ -94,6 +104,37 @@ function run_step_failed(){
         --run_step_name "${run_step}" \
         --run_step_status "Failed" \
         --run_step_end_time "Now"
+}
+
+function send_commit(){
+    echo "Sending commit information"
+    ./faros_event.sh CI \
+        --commit "$FAROS_VCS_SOURCE://$FAROS_VCS_ORG/$FAROS_VCS_REPO/$commit_sha" \
+
+    curl -X 'POST' \
+      "$FAROS_URL/graphs/$FAROS_GRAPH/revisions" \
+      -H "Accept: application/json" \
+      -H "Content-Type: application/json" \
+      -H "Authorization: $FAROS_API_KEY" \
+      -d '{
+        "origin": "Faros_Script_Event",
+        "entries": [
+          {
+            "vcs_Commit": {
+              "sha": "'$commit_sha'",
+              "message": "'$commit_message'",
+              "createdAt": '$NOW',
+              "repository": {
+                "name": "'$FAROS_VCS_REPO'",
+                "organization": {
+                  "uid": "'$FAROS_VCS_ORG'",
+                  "source": "'$FAROS_VCS_SOURCE'"
+                }
+              }
+            }
+          }
+        ]
+      }'
 }
 
 function main(){
