fade5700209dca33013ce70c390ba669e2ff07d3
Chris Wu <chris@faros.ai>
2023-01-23 12:33:36 -0800

    Setup_hooks script and template post-checkout

--- setup_hooks.sh
+++ setup_hooks.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Settings up git hooks..."
+echo "Setting up git hooks..."
+
+root=$(git rev-parse --show-toplevel)
+hooks_src_path="$root/git-hooks"
+hooks=$(ls "${hooks_src_path}")
+
+if [ -z "$hooks" ]; then
+    echo "No git hooks found"
+    exit 0
+fi
+
+cd "$root"/.git/hooks || exit
+
+for hook_path in "${hooks_src_path}"/*; do
+    hook=$(basename "$hook_path")
+    echo "Setting up $hook hook"
+    ln -sf "$hook_path" "$hook"
+done
+
 echo "Done."
